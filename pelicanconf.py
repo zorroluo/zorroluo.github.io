32c47a03b196e618b23b232a4135edd99a1a83c4
Zorro_Luo <zorro.luo@qq.com>
2026-08-30 20:05:13 +0800

    切换回 pelican 默认主题

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,7 +8,8 @@ TIMEZONE = 'Asia/Shanghai'
 
 DEFAULT_LANG = 'zh'
 
-THEME = "themes/pelican-alchemy/alchemy"
+# 注销这个主题，就会恢复到默认的主题
+# THEME = "themes/pelican-alchemy/alchemy"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
